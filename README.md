3cb1e67bca19fd251dace15bc28a0aac5fa6b895
TACHIBANA SHO <JP21988@w010D71704054m.local>
2017-10-25 14:40:52 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Deploy to Heroku and add Config Variables.
+
+* CHANNEL_SECRET => YOUR_CHANNEL_SECRET
+* CHANNEL_ACCESS_TOKEN => YOUR_CHANNEL_ACCESS_TOKEN
+
+Get them at [LINE Developers site](https://developers.line.me/), then set endpoint to deployed URL to run.
---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
+$signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+
+$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+foreach ($events as $event) {
+
+  $profile = $bot->getProfile($event->getUserId())->getJSONDecodedBody();
+  $displayName = $profile['displayName'];
+
+  if ($event instanceof \LINE\LINEBot\Event\MessageEvent) {
+    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+      if($event->getText() === 'こんにちは') {
+        $bot->replyMessage($event->getReplyToken(),
+          (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
+            ->add(new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 17))
+            ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('こんにちは！' . $displayName . 'さん'))
+        );
+      } else {
+        $bot->replyMessage($event->getReplyToken(),
+          (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
+            ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('「こんにちは」と呼びかけて下さいね！'))
+            ->add(new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 4))
+        );
+      }
+    }
+    continue;
+  }
+}
+
+?>
